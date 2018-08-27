ee248cf137fcf7824fd4466f84e8371211294a9f
Bruno Horta <brunohorta@Brunos-MacBook-Pro-2.local>
2018-08-27 11:43:15 +0100

    no message

--- Código/Teste_Motor_Estores/Teste_Motor_Estores.ino
+++ Código/Teste_Motor_Estores/Teste_Motor_Estores.ino
@@ -1,20 +1,31 @@
 void setup() {
   // put your setup code here, to run once:
 pinMode(4,OUTPUT);
+digitalWrite(4,HIGH);
 pinMode(5,OUTPUT);
-
+digitalWrite(5,HIGH);
 }
 
 void loop() {
  // put your main code here, to run repeatedly:
-delay(3000);
-digitalWrite(4,HIGH);
-digitalWrite(5,LOW);
-delay(3000);
-digitalWrite(4,LOW);
+delay(10000);
+
+
 digitalWrite(5,HIGH);
-delay(3000);
+delay(200);
 digitalWrite(4,LOW);
+
+delay(10000);
+
+
+digitalWrite(4,HIGH);
+delay(200);
 digitalWrite(5,LOW);
+
+
+
+delay(10000);
+digitalWrite(4,HIGH);
+digitalWrite(5,HIGH);
 
 }
